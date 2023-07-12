61d37eb6a95b05d4ff9ce697b8b34682c8943998
Ryan Anderson <ryan@Ryans-MacBook-Air.local>
2023-07-12 15:44:34 -0700

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Ryan Anderson's Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -55,7 +55,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
         </a>
       </footer>
@@ -111,5 +111,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
